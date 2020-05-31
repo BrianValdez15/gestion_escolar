2d913e6a099b3f8faad68c5b1eb8ba612d2a7cc4
YairReyes <66081359+YairReyes@users.noreply.github.com>
2020-05-31 15:17:53 -0500

    Create alumno.java

---
+++ gestion_escolar/alumno.java
@@ -0,0 +1,24 @@
+public class alumno {
+    
+   String Correo;
+   String Nombre;
+    String Alergias;
+   String Contraseña;
+   int TelPadre;
+   
+    public void Correo() { 
+    String Correo;
+    }
+    public void Nombre() { 
+    String Nombre;
+    }
+    public void Contraseña() { 
+    String Contraseña;
+    }
+    public void Alergias() { 
+    String Alergias;
+    }
+    public void TelPadre(){
+    int Tel;
+    }
+}
