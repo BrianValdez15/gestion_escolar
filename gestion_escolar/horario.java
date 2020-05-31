81db2902be5d93b23832332cc60bb4fff7d93368
YairReyes <66081359+YairReyes@users.noreply.github.com>
2020-05-31 15:25:45 -0500

    Create horario.java

---
+++ gestion_escolar/horario.java
@@ -0,0 +1,15 @@
+public class Horario{
+  
+   String Dia;
+   double Inicio,Termino;
+   
+    public void Dia() { 
+    String Dia;
+    }
+    public void Inicio() { 
+    double Inicio;
+    }
+    public void Termino() { 
+    double Termino;
+   }
+}
