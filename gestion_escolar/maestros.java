ea601a31c1640dfffe92c06cd8c239c1f649102f
YairReyes <66081359+YairReyes@users.noreply.github.com>
2020-05-31 15:13:03 -0500

    Create:maestros.java

---
+++ gestion_escolar/maestros.java
@@ -0,0 +1,21 @@
+public class Maestro {
+    
+   String Correo;
+   String Nombre;
+   String Contraseña;
+   int Tel;
+   
+    public void Correo() { 
+    String Correo;
+    }
+    public void Nombre() { 
+    String Nombre;
+    }
+    public void Contraseña() { 
+    String Contraseña;
+    }
+   
+    public void Tel(){
+    int Tel;
+    }
+}
