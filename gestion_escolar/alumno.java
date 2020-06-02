69a3e4c8daec07ec03ccda4c890964721a13752a
YairReyes <66081359+YairReyes@users.noreply.github.com>
2020-06-02 12:29:46 -0500

    Update alumnos.java

--- gestion_escolar/alumno.java
+++ gestion_escolar/alumno.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class alumno {
     
    String Correo;
@@ -8,17 +9,28 @@ public class alumno {
    
     public void Correo() { 
     String Correo;
+       JOptionPane.showMessageDialog(null, "brian@gmail.com");
     }
     public void Nombre() { 
     String Nombre;
+            JOptionPane.showMessageDialog(null, "Brian Valdez Garcia");
     }
     public void Contraseña() { 
     String Contraseña;
+         Scanner entrada = new Scanner(System.in);
     }
     public void Alergias() { 
     String Alergias;
+        JOptionPane.showMessageDialog(null, "Soy alergico al amor ");
     }
     public void TelPadre(){
     int Tel;
+        JOptionPane.showMessageDialog(null, "El telefono del tutor es: "+"598679");
     }
+     public void mostrar(){
+      System.out.print(Nombre);
+          System.out.print(Correo);
+          System.out.print(Alergias);
+          System.out.print(TelPadre);
+     }
 }
